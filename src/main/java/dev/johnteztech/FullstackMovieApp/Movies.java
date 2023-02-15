495bde0be65e4beb912e15014462e9e56d7ef6f9
john kaiser <52598487+johnkaizer@users.noreply.github.com>
2023-02-15 21:52:11 +0300

    Backend Done

--- src/main/java/dev/johnteztech/FullstackMovieApp/Movies.java
+++ src/main/java/dev/johnteztech/FullstackMovieApp/Movies.java
@@ -1,12 +1,19 @@
 package dev.johnteztech.FullstackMovieApp;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
-@Document(collation = "movies")
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movies {
     @Id
     private ObjectId id;
@@ -17,5 +24,7 @@ public class Movies {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewsId;
 
 }
